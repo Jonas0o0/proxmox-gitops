1ab27714af4d381bf6de73f2c7f15e79030cd3dc
Jonas0o0 <jonas.facon@proton.me>
2026-09-19 17:47:20 +0200

    fix(terraform): corriger le datastore_id de la template debian

--- terraform/environments/production/bootstrap/main.tf
+++ terraform/environments/production/bootstrap/main.tf
@@ -26,7 +26,7 @@ resource "proxmox_virtual_environment_vm" "debian13" {
   }
 
   disk {
-    datastore_id = var.backup_storage
+    datastore_id = var.storage
     file_id      = proxmox_download_file.debian13.id
     interface    = "scsi0"
     discard      = "on"
